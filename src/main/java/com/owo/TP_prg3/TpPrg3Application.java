e9ac5178158241d1b0e1493b22266c454eb6a61d
F3LL5 <dylanolivera565@gmail.com>
2025-05-30 16:10:32 -0300

    cambiamos el proyecto a spring

---
+++ src/main/java/com/owo/TP_prg3/TpPrg3Application.java
@@ -0,0 +1,13 @@
+package com.owo.TP_prg3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPrg3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpPrg3Application.class, args);
+	}
+
+}
