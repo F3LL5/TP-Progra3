f20bd4749063a02010b011a4f2955d74e7362e20
F3LL5 <143289294+F3LL5@users.noreply.github.com>
2025-06-14 23:41:42 -0300

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -3,14 +3,14 @@ create database if not exists comercio;
 use comercio;
 
 create table if not exists items (
-	item_id int auto_increment primary key,
+	item_id bigint auto_increment primary key,
 	nombre varchar(100) not null,
 	categoria varchar(100) not null,
 	costo decimal(10,2) not null
 );
 
 create table entidades(
-	entidad_id int auto_increment primary key,
+	entidad_id bigint auto_increment primary key,
 	nombre varchar(100),
 	tipo_entidad varchar(100) not null,
 	rol varchar(100) not null,
@@ -19,19 +19,19 @@ create table entidades(
 );
 
 create table if not exists puestos(
-	puesto_id int auto_increment primary key,
+	puesto_id bigint auto_increment primary key,
 	nombre varchar(100) not null,
-	duenio_id int not null,
+	duenio_id bigint not null,
 	foreign key(duenio_id) references entidades(entidad_id)
 	on delete cascade
 	on update cascade
 );
 
 create table if not exists inventario_puesto (
-	inventario_id int auto_increment primary key,
+	inventario_id bigint auto_increment primary key,
 	cantidad int not null,
-	puesto_id int,
-	item_id int,
+	puesto_id bigint,
+	item_id bigint,
 	stock_min int not null,
 	precio_venta decimal(10,2) not null,
 	foreign key(puesto_id) references entidades(entidad_id),
@@ -41,33 +41,33 @@ create table if not exists inventario_puesto (
 );
 
 create table if not exists cuenta_bancaria(
-	cuenta_bancaria_id int auto_increment primary key,
-	entidad_id int,
+	cuenta_bancaria_id bigint auto_increment primary key,
+	entidad_id bigint,
 	saldo decimal (10,2) not null,
 	foreign key(entidad_id) references entidades(entidad_id) 
 );
 
 create table if not exists transacciones(
-	transaccion_id int auto_increment primary key,
+	transaccion_id bigint auto_increment primary key,
 	tipo varchar(100) not null,
 	fecha timestamp default current_timestamp,
 	monto decimal(10,2) not null,
-	cuenta_origen_id int,
-	cuenta_destino_id int,
+	cuenta_origen_id bigint,
+	cuenta_destino_id bigint,
 	foreign key(cuenta_origen_id) references cuenta_bancaria(cuenta_bancaria_id),
 	foreign key(cuenta_destino_id) references cuenta_bancaria(cuenta_bancaria_id) 
 );
 
 create table if not exists pedidos (
-	pedido_id int auto_increment primary key,
-	transaccion_id int,
+	pedido_id bigint auto_increment primary key,
+	transaccion_id bigint,
 	foreign key(transaccion_id) references transacciones(transaccion_id)
 );
 
 create table if not exists detalles_pedido (
-	detalle_pedido_id int auto_increment primary key,
-    pedido_id int not null,
-	item_id int not null,
+	detalle_pedido_id bigint auto_increment primary key,
+    pedido_id bigint not null,
+	item_id bigint not null,
 	cantidad int not null,
 	precio_total decimal(10,2) not null,
 	foreign key(pedido_id) references pedidos(pedido_id),
