b9b42eecc3ae4c431220d52d656f691e4fb0208e
F3LL5 <143289294+F3LL5@users.noreply.github.com>
2025-06-15 22:23:59 -0300

    Create MenuPedidos.java

---
+++ src/main/java/com/owo/TP_prg3/Front/Menu/MenuPedidos.java
@@ -0,0 +1,119 @@
+package com.owo.TP_prg3.Front.Menu;
+
+import com.owo.TP_prg3.Front.HttpService;
+import com.owo.TP_prg3.Front.Utilidades.Escaner;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class MenuPedidos {
+
+    //Atributos
+    private static final String API_URL = "http://localhost:8080/api/pedidos";
+    private final String authHeader;
+    private final Scanner scanner = new Scanner(System.in);
+
+    //Constructor
+    public MenuPedidos(String authHeader) {this.authHeader = authHeader;}
+
+    //Menu
+    public void gestionar() throws IOException, InterruptedException {
+        String opcion;
+        do {
+            mostrarMenu();
+            opcion = Escaner.stringValido(scanner);
+            switch (opcion) {
+                case "1" -> obtenerTodas();
+                case "2" -> buscarPorId();
+                case "3" -> agregar();
+                case "4" -> eliminar();
+                case "5" -> modificar();
+
+                case "0" -> {} // Salir
+                default -> System.out.println("Opción no válida. Inténtelo de nuevo.");
+            }
+        } while (!opcion.equals("0"));
+    }
+
+
+    private void mostrarMenu() {
+        System.out.print("""
+                \n--- MENÚ DE GESTIÓN DE PEDIDO ---
+                OPCIONES:
+                1. Obtener todos
+                2. Buscar por id
+                3. Agregar
+                4. Eliminar
+                5. Modificar
+                
+                0. Salir
+                Ingrese la opción:""");
+    }
+
+    //Metodos
+    //GET
+    private void obtenerTodas() throws IOException, InterruptedException {
+        System.out.println("\n--- Obteniendo todas los pedido... ---");
+        HttpService.realizarPeticion("GET", API_URL, authHeader, null);
+    }
+
+    private void buscarPorId() throws IOException, InterruptedException {
+        System.out.print("Ingrese el ID de la entidad: ");
+        Integer id = Escaner.enteroValido(scanner);
+        HttpService.realizarPeticion("GET", API_URL + "/" + id, authHeader, null);
+    }
+
+    //POST
+    private void agregar() throws IOException, InterruptedException {
+        System.out.println("\n--- Registrar pedido ---");
+
+        System.out.print("Ingrese el id de la transaccion: ");
+        int transaccionId = Escaner.enteroValido(scanner);
+
+        String jsonBody =
+                   "{" +
+                       "\"transaccionId\":" + transaccionId  +
+                   "}";
+
+        HttpService.realizarPeticion("POST", API_URL, authHeader, jsonBody);
+    }
+
+    //DELETE
+    private void eliminar() throws IOException, InterruptedException {
+        System.out.print("Ingrese el ID del pedido a eliminar: ");
+        Integer id = Escaner.enteroValido(scanner);
+        HttpService.realizarPeticion("DELETE", API_URL + "/" + id, authHeader, null);
+    }
+
+    //PATCH
+    private void modificar() throws IOException, InterruptedException {
+        System.out.print("Ingrese el ID del pedido a modificar: ");
+        Integer id = Escaner.enteroValido(scanner);
+        System.out.println();
+
+        System.out.print("""
+                ATRIBUTO A MODIFICAR:
+                1. ID de la transaccion
+                0. Cancelar
+                Ingrese una opcion:""");
+        Integer opcion = Escaner.enteroValido(scanner);
+        if (opcion != 0 ) System.out.print("Ingrese el nuevo valor: ");
+
+        String jsonBody = "";
+        switch (opcion){
+            case 1 -> {
+                int transaccionId = Escaner.enteroValido(scanner);
+                jsonBody = "{\"transaccionId\":"  + transaccionId + "}";
+            }
+            case 0 -> {}
+            default -> {
+                System.out.println("Opcion no valida.");
+                return;
+            }
+        }
+
+        HttpService.realizarPeticion("PATCH", API_URL + "/" + id, authHeader, jsonBody);
+    }
+
+}
