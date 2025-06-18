62220d2bf77239e622dfd3df03e7db86d8ad67f7
DloguPia <162518515+DloguPia@users.noreply.github.com>
2025-06-18 16:45:18 -0300

    Agregar las tablas a Historiales

--- src/main/java/com/owo/TP_prg3/Front/Menu/MenuAuditoria/MenuHistorial_Duenio.java
+++ src/main/java/com/owo/TP_prg3/Front/Menu/MenuAuditoria/MenuHistorial_Duenio.java
@@ -1,8 +1,16 @@
 package com.owo.TP_prg3.Front.Menu.MenuAuditoria;
 
+import com.owo.TP_prg3.Auditoria.DuenioHistorial.dto.DuenioHistorialDTO;
+import com.owo.TP_prg3.Auditoria.DuenioHistorial.modelo.DuenioHistorial;
+import com.owo.TP_prg3.Clases.Item.dto.ItemDTO;
+import com.owo.TP_prg3.Excepciones.Handler.HandlerResponse;
 import com.owo.TP_prg3.Front.HttpService;
 import com.owo.TP_prg3.Front.Utilidades.Escaner;
+import com.owo.TP_prg3.Front.Utilidades.FlipTableHelper;
+
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class MenuHistorial_Duenio {
@@ -26,8 +34,6 @@ public class MenuHistorial_Duenio {
                 case "1" -> obtenerTodas();
                 case "2" -> buscarPorId();
 
-                case "1.2" -> listado();
-
                 case "0" -> {} // Salir
                 default -> System.out.println("Opción no válida. Inténtelo de nuevo.");
             }
@@ -41,26 +47,46 @@ public class MenuHistorial_Duenio {
                 1. Obtener todos
                 2. Buscar por id
                 
-                1.2 Listado
                 0. Salir
                 Ingrese la opción:""");
     }
 
     //GET
     private void obtenerTodas() throws IOException, InterruptedException {
-        System.out.println("\n--- Obteniendo todos los cambios... ---");
-        HttpService.realizarPeticion("GET", API_URL, authHeader, null);
+        System.out.println("\n--- Obteniendo todos los historiales de duenios... ---");
+
+        Optional<Object> result = HandlerResponse.handleResponse(
+                HttpService.realizarPeticion("GET", API_URL, authHeader, null),
+                DuenioHistorialDTO.class,
+                "Historial obtenido exitosamente.",
+                "No se pudieron obtener los historiales."
+        );
+
+        result.ifPresent(obj -> {
+            List<DuenioHistorialDTO> historiales = (List<DuenioHistorialDTO>) obj;
+            if (!historiales.isEmpty()) {
+                FlipTableHelper.imprimir(historiales);
+            } else {
+                System.out.println("No se encontraron resultados.");
+            }
+        });
     }
 
     private void buscarPorId() throws IOException, InterruptedException {
-        System.out.print("Ingrese el ID del cambio: ");
+        System.out.print("Ingrese el ID del historial: ");
         Integer id = Escaner.enteroValido(scanner);
-        HttpService.realizarPeticion("GET", API_URL + "/" + id, authHeader, null);
-    }
 
-    private void listado() throws IOException, InterruptedException {
-        System.out.println("\n--- Obteniendo todos los cambios... ---");
-        HttpService.realizarPeticion("GET", API_URL + "/listado", authHeader, null);
+        Optional<Object> result = HandlerResponse.handleResponse(
+                HttpService.realizarPeticion("GET", API_URL + "/" + id, authHeader, null),
+                DuenioHistorialDTO.class,
+                "Historial encontrado:",
+                "No se encontró el historal con ID " + id + "."
+        );
+
+        result.ifPresent(obj -> {
+            DuenioHistorialDTO historial = (DuenioHistorialDTO) obj;
+            FlipTableHelper.imprimir(List.of(historial));
+        });
     }
 
 }
--- src/main/java/com/owo/TP_prg3/Front/Menu/MenuAuditoria/MenuHistorial_InventarioCosto.java
+++ src/main/java/com/owo/TP_prg3/Front/Menu/MenuAuditoria/MenuHistorial_InventarioCosto.java
@@ -1,9 +1,15 @@
 package com.owo.TP_prg3.Front.Menu.MenuAuditoria;
 
+import com.owo.TP_prg3.Auditoria.DuenioHistorial.dto.DuenioHistorialDTO;
+import com.owo.TP_prg3.Auditoria.InventarioCostoHistorial.dto.InventarioCostoHistorialDTO;
+import com.owo.TP_prg3.Excepciones.Handler.HandlerResponse;
 import com.owo.TP_prg3.Front.HttpService;
 import com.owo.TP_prg3.Front.Utilidades.Escaner;
+import com.owo.TP_prg3.Front.Utilidades.FlipTableHelper;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class MenuHistorial_InventarioCosto {
@@ -40,7 +46,6 @@ public class MenuHistorial_InventarioCosto {
                 1. Obtener todos
                 2. Buscar por id
                 
-                1.2 Listado
                 1.3 filtrarYordenar
                 0. Salir
                 Ingrese la opción:""");
@@ -49,14 +54,42 @@ public class MenuHistorial_InventarioCosto {
     //Metodos
     //GET
     private void obtenerTodas() throws IOException, InterruptedException {
-        System.out.println("\n--- Obteniendo todas las cambios... ---");
-        HttpService.realizarPeticion("GET", API_URL, authHeader, null);
+        System.out.println("\n--- Obteniendo todos los historiales de inventario costo... ---");
+
+        Optional<Object> result = HandlerResponse.handleResponse(
+                HttpService.realizarPeticion("GET", API_URL, authHeader, null),
+                InventarioCostoHistorialDTO.class,
+                "Historial obtenido exitosamente.",
+                "No se pudieron obtener los historiales."
+        );
+
+        result.ifPresent(obj -> {
+            List<InventarioCostoHistorialDTO> historiales = (List<InventarioCostoHistorialDTO>) obj;
+            if (!historiales.isEmpty()) {
+                FlipTableHelper.imprimir(historiales);
+            } else {
+                System.out.println("No se encontraron resultados.");
+            }
+        });
     }
 
     private void buscarPorId() throws IOException, InterruptedException {
-        System.out.print("Ingrese el ID del cambio: ");
+        System.out.print("Ingrese el ID del historial: ");
         Integer id = Escaner.enteroValido(scanner);
-        HttpService.realizarPeticion("GET", API_URL + "/" + id, authHeader, null);
+
+        Optional<Object> result = HandlerResponse.handleResponse(
+                HttpService.realizarPeticion("GET", API_URL + "/" + id, authHeader, null),
+                InventarioCostoHistorialDTO.class,
+                "Historial encontrado:",
+                "No se encontró el historal con ID " + id + "."
+        );
+
+        result.ifPresent(obj -> {
+            InventarioCostoHistorialDTO historial = (InventarioCostoHistorialDTO) obj;
+            FlipTableHelper.imprimir(List.of(historial));
+        });
     }
+
+
 
 }
