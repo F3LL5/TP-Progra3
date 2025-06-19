807b62a6064ba39b911de8e21635128f7445846d
F3LL5 <143289294+F3LL5@users.noreply.github.com>
2025-06-19 00:01:59 -0300

    filtrado de transacciones

--- src/main/java/com/owo/TP_prg3/Configuracion/SecurityConfig.java
+++ src/main/java/com/owo/TP_prg3/Configuracion/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/transacciones/puesto/*").hasAnyRole("DUENO_PUESTO", "ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/transacciones/*/puesto/*").hasAnyRole("DUENO_PUESTO", "ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/transacciones/filtrarYOrdenar*").hasAnyRole("DUENO_PUESTO", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/transacciones/puesto/*/filtrarYOrdenar*").hasAnyRole("DUENO_PUESTO", "ADMIN")
 
                         // Pedidos
                         .requestMatchers(HttpMethod.GET, "/api/pedidos").hasAnyRole("DUENO_PUESTO", "ADMIN")
